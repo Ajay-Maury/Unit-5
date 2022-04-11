25ce23fb0cde5fd257e1af6e63a64a4ae7700f81
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-04-11 19:18:36 +0530

    State Management

---
+++ Assignments/State Management/react-todos/src/components/TodoInput.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const TodoInput = ({ getData }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        type="text"
+        placeholder="Enter Todo"
+      />
+
+      <button
+        onClick={() => {
+          getData(text);
+        }}
+      >
+        Add todo
+      </button>
+    </div>
+  );
+};
