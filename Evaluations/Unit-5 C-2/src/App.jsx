60c265e62084aabad35850206a62b564ecb86cfa
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-04-18 17:10:11 +0530

    C-2

--- Evaluations/Unit-5 C-2/src/App.jsx
+++ Evaluations/Unit-5 C-2/src/App.jsx
@@ -1,45 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import { AddStudent } from "./components/AddStudent";
+import { ShowStudents } from "./components/ShowStudents";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [status, setStatus] = useState(false);
+  function statusHandler() {
+    setStatus(!status);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <button className="togglebtn" onClick={() => statusHandler()}>
+        {status ? "Add a new student" : "Go to students list"}
+      </button>
+      {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
+      {/* make sure the table is shown initially, do not show form initially */}
+      {/* make sure to show either of them do not both together */}
+
+      {status ? <ShowStudents /> : <AddStudent />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Evaluations/Unit-5 C-2/src/components/ShowStudents.jsx
@@ -0,0 +1,152 @@
+import { useState, useEffect } from "react";
+export const ShowStudents = () => {
+  const [students, setStudents] = useState([]);
+  const [sortBy, setSortBy] = useState("first_name");
+  const [order, setOrder] = useState("Ascending");
+
+  useEffect(() => {
+    getdata();
+
+    return function cleanup() {};
+  }, []);
+
+  async function getdata() {
+    const data = await fetch(`http://localhost:8080/students`).then((d) =>
+      d.json()
+    );
+    setStudents(data);
+    // console.log(data);
+  }
+
+  // function handelchagne2(e) {
+  //   setSortBy(e.target.value);
+  // }
+  // function handelchagne3(e) {
+  //   setOrder(e.target.value);
+  // }
+  // const handelchagne1 = (e) => {
+  //   let a = e.target.value;
+  //   if (a == "Ascending") {
+  //     var s = students.sort((a, b) => {
+  //       return a[sortBy] - b[sortBy];
+  //     });
+  //   } else {
+  //     var s = students.sort((a, b) => {
+  //        return a[sortBy] - b[sortBy];
+  //     });
+  //   }
+  //   console.log(students);
+  //   setStudents(s)
+  //   // setfilter(newarray);
+  // };
+
+  return (
+    <div>
+      {console.log(students)}
+      <div className="controls">
+        <div>
+          Sort By:{" "}
+          <select
+            // select dropdown needs both value and onChange
+            className="sortby"
+            // onChange={handelchagne3(e)}
+          >
+            <option value="first_name">First Name</option>
+            <option value="gender">Gender</option>
+            <option value="age">Age</option>
+            <option value="tenth_score">10th Score</option>
+            <option value="twelth_score">12th Score</option>
+          </select>
+        </div>
+        <div>
+          Order:
+          <select
+            // select dropdown needs both value and onChange
+            // onChange={handelchagne2(e)}
+            className="sortorder"
+          >
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
+        <button className="sort"
+          // onClick={handelchagne1(e)}
+        >
+          sort
+        </button>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>10th Score</th>
+            <th>12th Score</th>
+            <th>Branch</th>
+          </tr>
+        </thead>
+        <tbody className="tbody">
+          {/* populate all rows like below: */}
+          {students.map((elem) => (
+            <tr className="row" key={elem.id}>
+              <td className="first_name">{elem.first_name}</td>
+              <td className="last_name">{elem.last_name}</td>
+              <td className="email">{elem.email}</td>
+              <td className="gender">{elem.gender}</td>
+              <td className="age">{elem.age}</td>
+              <td className="tenth_score">{elem.tenth_score}</td>
+              <td className="twelth_score">{elem.twelth_score}</td>
+              <td className="preferred_branch">{elem.preferred_branch}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+// // import { useEffect, useState } from "react";
+// import "./product.css";
+
+// export const Product = () => {
+//   const [product, setproduct] = useState([]);
+//   const[filter,setfilter]=useState([])
+
+//   useEffect(() => {
+//     // getdata();
+//   }, []);
+
+//   const getdata = async () => {
+//     fetch(`http://localhost:8080/user`)
+//       .then((d) => d.json())
+//       .then((data) => {
+//         setproduct(data);
+//         setfilter(data)
+//         // console.log(data);
+//       });
+//   };
+//   const handelchagne = (e) => {
+//    let a=e.target.value
+//    var newarray=product.filter((e)=>e.Rating==a)
+//    setfilter(newarray)
+//   };
+
+//   const handelchagne2 = (e) => {
+//     let a=e.target.value
+//     var newarray=product.filter((e)=>e.payment==a)
+//     setfilter(newarray)
+//    };
+
+//    const handelchagne3 = (e) => {
+//     let a=e.target.value
+//      if(a=="High"){
+//      var newarray=product.sort((a, b)=>{return a.cost02 - b.cost02})
+//      }else{
+//      var newarray=product.sort((a, b)=>{return b.cost02 - a.cost02})
+//      }
+//     console.log(newarray)
+//     setfilter(newarray)
+//    };
