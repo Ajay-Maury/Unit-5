ec0e5e4b5d691b3cc900b5c84bc604bbb72d8f91
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-05-02 15:20:29 +0530

    Unit-5 C-4

--- Evaluations/Unit-5-Eval-C-4/src/App.jsx
+++ Evaluations/Unit-5-Eval-C-4/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           }
         ></Route>
         <Route
-          path="/neworders"
+          path="/neworder"
           element={
             <ProtectedRoute>
               <NewOrder />
